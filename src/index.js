ad8d1d44ce780a66b7cd761b67af29aae120aacf
VladimirG91 <vladimir.glazkov91@gmail.com>
2022-07-28 17:30:16 +0600

    fix: chage for, del &&, change length

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
   let arr = bracketsConfig.flat(1).join('').match(/.{2}/g)
-  for (let i = 0; i < str.length + 40; i++) {
-    ;(str = str.replace(arr[0], '')) &&
-      (str = str.replace(arr[1], '')) &&
-      (str = str.replace(arr[2], '')) &&
-      (str = str.replace(arr[3], '')) &&
-      (str = str.replace(arr[4], ''))
+  for (let i = 0; i < str.length + 35; i++) {
+    str = str.replace(arr[0], '')
+    str = str.replace(arr[1], '')
+    str = str.replace(arr[2], '')
+    str = str.replace(arr[3], '')
+    str = str.replace(arr[4], '')
   }
   return str.length === 0
 }
