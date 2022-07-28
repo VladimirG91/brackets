1254e62877954c12e6be94537c208cb5042081e9
VladimirG91 <vladimir.glazkov91@gmail.com>
2022-07-28 13:06:48 +0600

    feat: 13 pass 7 fail

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const obj = {}
+  bracketsConfig.forEach((pair) => (obj[pair[1]] = pair[0]))
+  let values = Object.values(obj)
+  let stack = []
+  for (let i = 0; i < str.length; i++) {
+    let currentElement = str[i]
+    if (values.includes(currentElement)) {
+      stack.push(currentElement)
+    } else {
+      if (stack.length === 0) {
+        return false
+      }
+      let topElement = stack[stack.length - 1]
+      if (obj[currentElement] === topElement) {
+        stack.pop()
+      } else {
+        return false
+      }
+    }
+  }
+  return stack.length === 0
 }
